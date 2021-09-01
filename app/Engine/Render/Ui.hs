bd405445a3893e545864554441d34e44c3bd4e25
Sam Schweigel <s.schweigel@gmail.com>
2021-09-01 01:23:06 -0700

    OFinish drawPicture

--- app/Engine/Render/Ui.hs
+++ app/Engine/Render/Ui.hs
@@ -1,9 +1,138 @@
-module Engine.Render.Ui () where
+{-# LANGUAGE TemplateHaskell #-}
 
+module Engine.Render.Ui
+  ( compilePictureShader,
+    drawPicture,
+    Picture (..),
+    DrawEnv (DrawEnv),
+  )
+where
+
+import Codec.Picture
+import Control.Lens
+import Control.Monad.IO.Class
+import Data.Char
 import Graphics.GPipe
 
+data DrawEnv os = DrawEnv
+  { _envWindow :: Window os RGBAFloat (),
+    _envSize :: V2 Int
+  }
+
+makeLenses ''DrawEnv
+
 data Picture
-  = PRect !(V2 Int) !(V2 Int) !(V3 Float)
-  | PString !String
+  = PRect !(V2 Int)
+  | PString String
   | PTranslate !(V2 Int) !Picture
+  | PColour !(V4 Float) !Picture
   | PPictures ![Picture]
+
+newtype PictureShader os
+  = PictureShader (CompiledShader os (PictureEnv os))
+
+data PictureEnv os = PictureEnv
+  { _envDraw :: DrawEnv os,
+    _envPrims :: PrimitiveArray Triangles (B2 Float, B2 Float, B4 Float)
+  }
+
+makeLenses ''PictureEnv
+
+compilePictureShader :: ContextHandler ctx => ContextT ctx os IO (PictureShader os)
+compilePictureShader = do
+  Right (ImageY8 img) <- liftIO (readPng textureFile)
+  let imgSize = V2 (imageWidth img) (imageHeight img)
+  texture <- newTexture2D R8 imgSize 1
+  writeTexture2D texture 0 (V2 0 0) imgSize $
+    toListOf (imagePixels . to (\x -> if x > 0 then 1 else 0 :: Float)) img
+
+  PictureShader <$> compileShader do
+    prims <- toPrimitiveStream (view envPrims)
+    sampler <-
+      newSampler2D
+        (const (texture, SamplerNearest, (V2 Repeat Repeat, undefined)))
+    frags <-
+      rasterize
+        (\s -> (FrontAndBack, PolygonFill, ViewPort 0 (s ^. envDraw . envSize), DepthRange 0 1))
+        (fmap (\(V2 x y, uv, c) -> (V4 x y 0 1, (uv, c))) prims)
+
+    let frags' =
+          fmap
+            ( \(uv, V4 r g b a) ->
+                let x = sample2D sampler SampleAuto Nothing Nothing uv
+                 in V4 r g b (a * x)
+            )
+            frags
+    drawWindowColor
+      (\s -> (s ^. envDraw . envWindow, ContextColorOption blendingOptions (V4 True True True False)))
+      frags'
+
+drawPicture :: ContextHandler ctx => PictureShader os -> DrawEnv os -> Picture -> ContextT ctx os IO ()
+drawPicture (PictureShader shader) env pic = do
+  let verts = pictureVerts env pic
+  buf <- newBuffer (length verts)
+  writeBuffer buf 0 verts
+
+  render do
+    va <- newVertexArray buf
+    let prims = toPrimitiveArray TriangleList va
+    shader $ PictureEnv env prims
+
+pictureVerts :: DrawEnv os -> Picture -> [(V2 Float, V2 Float, V4 Float)]
+pictureVerts env (PColour col p) = pictureVerts env p & traverse . _3 .~ col
+pictureVerts env (PRect (V2 w h)) =
+  [ V2 0 0,
+    V2 0 h,
+    V2 w 0,
+    V2 w 0,
+    V2 0 h,
+    V2 w h
+  ]
+    & fmap \v -> (pixelToNDS env v, V2 (135 / 136) 0, 1)
+pictureVerts env (PString str) = stringVerts env str
+pictureVerts env (PTranslate pos p) =
+  fmap (_1 %~ (+ (fmap fromIntegral pos * (envPixelSize env & _2 %~ negate)))) (pictureVerts env p)
+pictureVerts env (PPictures p) = foldr (\x -> (pictureVerts env x ++)) [] p
+
+stringVerts env str =
+  let textureChar = 1 / fmap fromIntegral textureSize
+      charUV c = fmap fromIntegral (V2 (ord c `mod` 16) (ord c `div` 16)) * textureChar
+
+      V2 charX charY = charSize
+
+      charVerts :: V2 Int -> Char -> [(V2 Float, V2 Float)]
+      charVerts p c =
+        let V2 u v = charUV c
+            V2 u' v' = V2 u v + textureChar
+         in [ (V2 0 0, V2 u v),
+              (V2 0 charY, V2 u v'),
+              (V2 charX 0, V2 u' v),
+              (V2 charX 0, V2 u' v),
+              (V2 0 charY, V2 u v'),
+              (V2 charX charY, V2 u' v')
+            ]
+              & traverse . _1 %~ (+ p)
+              & traverse . _1 %~ pixelToNDS env
+
+      lineVerts y = concat . zipWith charVerts [V2 (x * charX) y | x <- [0 ..]]
+   in lineVerts 0 str & fmap \(p, uv) -> (p, uv, V4 1 1 1 1)
+
+envPixelSize env = 2 / fmap fromIntegral (env ^. envSize)
+
+pixelToNDS env (V2 x y) =
+  let V2 pixelX pixelY = envPixelSize env
+   in V2 (fromIntegral x * pixelX - 1) (- fromIntegral y * pixelY + 1)
+
+blendingOptions =
+  BlendRgbAlpha
+    (FuncAdd, FuncAdd)
+    (BlendingFactors SrcAlpha OneMinusSrcAlpha, BlendingFactors One OneMinusSrcAlpha)
+    1
+
+charSize :: V2 Int
+charSize = V2 8 15
+
+textureSize :: V2 Int
+textureSize = V2 17 16
+
+textureFile = "textures/fixedsys.png"
--- app/Main.hs
+++ app/Main.hs
@@ -15,13 +15,13 @@ import Data.Time.Clock
 import Engine.Logic
 import Engine.Player
 import Engine.Render.Bsp
-import Engine.Render.Text
+import Engine.Render.Ui
 import Engine.World
 import Graphics.GPipe
 import qualified Graphics.GPipe.Context.GLFW as GLFW
 import System.Environment (getArgs)
-import Prelude hiding ((.))
 import Text.Printf
+import Prelude hiding ((.))
 
 main :: IO ()
 main = do
@@ -64,7 +64,7 @@ renderer :: Input -> IORef World -> IO ()
 renderer input world = runContextT GLFW.defaultHandleConfig do
   win <-
     newWindow
-      (WindowFormatColor RGB8)
+      (WindowFormatColor RGBA8)
       ( (GLFW.defaultWindowConfig "sector f lambda complex")
           { GLFW.configSwapInterval = Just 1
           }
@@ -82,28 +82,29 @@ renderer input world = runContextT GLFW.defaultHandleConfig do
   shader <- compileShader do
     prims <- toPrimitiveStream (view _2)
     lookmat <- getUniform (\s -> (s ^. _3, 0))
-    col <- getUniform (\s -> (s ^. _4, 0))
+    V3 r g b <- getUniform (\s -> (s ^. _4, 0))
     frags <-
       rasterize
         (\e -> (FrontAndBack, PolygonLine 1, ViewPort 0 (e ^. _1), DepthRange 0 1))
         (fmap (\(V3 x y z) -> (lookmat !* V4 x y z 1, ())) prims)
 
     drawWindowColor
-      (const (win, ContextColorOption NoBlending (V3 True True True)))
-      (fmap (const col) frags)
+      (const (win, ContextColorOption NoBlending (V4 True True True False)))
+      (fmap (const (V4 r g b 0)) frags)
 
   matBuf :: Buffer _ (Uniform (M44 (B Float))) <- newBuffer 1
   colBuf :: Buffer _ (Uniform (B3 Float)) <- newBuffer 1
 
   let coordsm = V4 (V4 0 (-1) 0 0) (V4 0 0 1 0) (V4 (-1) 0 0 0) (V4 0 0 0 1)
 
-  textShader <- loadFont
+  pictureShader <- compilePictureShader
 
-  let rloop = do
+  let rloop t = do
         w <- liftIO (readIORef world)
 
         Just (width, height) <- GLFW.getWindowSize win
         let aspect = fromIntegral width / fromIntegral height
+            viewport = V2 width height
 
         writeBuffer matBuf 0 [perspective (pi / 2) aspect 1 10000 !*! coordsm !*! playerMat w]
         writeBuffer colBuf 0 [V3 1 1 1]
@@ -111,15 +112,24 @@ renderer input world = runContextT GLFW.defaultHandleConfig do
         render do
           clearWindowColor win 0
           prims <- renderBsp bspGpu (w ^. playerPos)
-          shader (V2 width height, prims, matBuf, colBuf)
+          shader (viewport, prims, matBuf, colBuf)
 
-        drawString textShader win (V2 width height) (V3 1 0 0) (V2 10 10)
-          (printf "pos: %5.0f %5.0f %5.0f" (w ^. playerPos . _x) (w ^. playerPos . _y) (w ^. playerPos . _z))
-        drawString textShader win (V2 width height) (V3 0 1 0) (V2 10 25)
-          (printf "ang: %3.2f %3.2f" (w ^. playerAng . _x) (w ^. playerAng . _y))
+        t' <- liftIO getCurrentTime
+        let dt = diffUTCTime t' t
+            fps = realToFrac (1 / dt) :: Float
+
+        drawPicture pictureShader (DrawEnv win viewport) $
+          PTranslate
+            (V2 10 10)
+            ( PPictures
+                [ PColour (V4 0 0 0.5 0.8) (PRect (V2 52 17)),
+                  PTranslate (V2 1 1) (PString (printf "%2.0f fps" fps))
+                ]
+            )
 
         swapWindowBuffers win
         close <- GLFW.windowShouldClose win
-        unless (close == Just True) rloop
+        unless (close == Just True) (rloop t')
 
-  rloop
+  t <- liftIO getCurrentTime
+  rloop t
