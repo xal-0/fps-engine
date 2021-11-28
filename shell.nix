d3be0885e8a88e9ad48a1265476cbd93c1ac1d40
Sam Schweigel <s.schweigel@gmail.com>
2021-11-27 23:59:47 -0800

    Update nix infrastructure

--- shell.nix
+++ shell.nix
@@ -1,13 +1,22 @@
 { pkgs ? import <nixpkgs> { } }:
 
-(import ./. { inherit pkgs; }).shellFor {
+let
+  hpkgs = pkgs.haskellPackages.extend (self: super:
+    {
+      GPipe-Core = pkgs.haskell.lib.doJailbreak super.GPipe-Core;
+      GPipe-GLFW4 = pkgs.haskell.lib.dontCheck super.GPipe-GLFW4;
+    }
+    // (pkgs.haskellPackages.packageSourceOverrides { fps-engine = ./.; }) self
+    super);
+
+in hpkgs.shellFor {
   packages = p: [ p.fps-engine ];
   withHoogle = true;
   buildInputs = with pkgs; [
     cabal-install
     renderdoc
     ghcid
-    haskellPackages.hoogle
+    haskell-language-server
     haskellPackages.threadscope
     haskellPackages.hp2pretty
     haskellPackages.profiteur
